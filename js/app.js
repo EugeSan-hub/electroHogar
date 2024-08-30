94cf8298493cbeb207fa4cda115d7938bb828fc1
Tomacho <tomacho28.th@gmail.com>
2024-08-29 22:35:20 -0300

    falta corregir la funcion

--- js/app.js
+++ js/app.js
@@ -10,23 +10,6 @@ const listaProductos =
 // dibujar el objeto en la card
 
 
-const productoBuscado = listaProductos.find((producto) => producto.id === parametroId)
+const productoBuscado = listaProductos.find((elemento) => elemento.id === parametroId);
+console.log(productoBuscado);
 
-const card1 = document.getElementById("#contenedorCard")
-card1.innerHTML = `<article
-                class="col-10 col-md-5 col-lg-3 mx-2 my-3 border border-2 rounded-3 p-3"
-                id="contenedorCard">
-                <a href="./categoriaCocinas.html"
-                  ><img
-                    src="../img/imgJu/cocinas/Orbis/Orbis1.png"
-                    alt="Cocina Orbis color Acero"
-                    class="img-fluid rounded-2"
-                /></a>
-                <p class="text-center mt-3 py-2">
-                  <span
-                    >Cocina Orbis 96eac4 Combinada Color Acero<br />
-                    Horno eléctrico Hornalla Gas</span
-                  >
-                </p>
-                <p class="fw-bold text-center">${productoBuscado.precio}</p>
-              </article>'`
