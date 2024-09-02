bb94fe5e6fe8f58d9f92b36f1f98db65fb0da237
tomacho <tomacho28.th@gmail.com>
2024-09-02 08:42:33 -0300

    terminamos logica y estetica de verprodcto

--- js/app.js
+++ js/app.js
@@ -4,7 +4,9 @@ const listaProductos = JSON.parse(localStorage.getItem("ListaProductosKey")) || 
 
 // Seleccionar el contenedor donde se dibujarán las tarjetas
 const contenedorCards = document.querySelector("#contenedorCard");
-
+window.verDetalle = (id) => {
+  window.location.href = "/electroHogar/index.html?id=" + id;
+};
 if (contenedorCards) {
   // Iterar sobre todos los productos y crear una tarjeta para cada uno
   listaProductos.forEach((producto) => {
@@ -36,17 +38,17 @@ if (parametroId) {
     contenedorDetalle.innerHTML = `
         <div class="row">
           <div class="col-sm-12 col-md-6 col-xl-8">
-            <img src="${productoEncontrado.imagen}" alt="${productoEncontrado.nombre}" class="img-fluid h-100" />
+            <img src="${productoEncontrado.imagen}" alt="${productoEncontrado.nombre}" class="img-fluid" />
           </div>
           <div class="col-sm-12 col-md-6 col-xl-4 rounded-2">
-            <h2 class="text-dark">${productoEncontrado.nombre}</h2>
-            <h6 class="text-secondary mb-3 fw-lighter fs-5">
+            <h2 class="text-light">${productoEncontrado.nombre}</h2>
+            <h6 class="text-light mb-3 fw-lighter fs-5">
               Venta de electrodomésticos verificada
               <i class="bi bi-patch-check-fill text-primary"></i>
             </h6>
-            <h3 class="text-dark">$${productoEncontrado.precio}</h3<h5 class="text-dark mt-3">${productoEncontrado.descripcion}</h5>
+            <h3 class="text-light">$${productoEncontrado.precio}</h3><h5 class="text-light mt-3">${productoEncontrado.descripcion}</h5>
            <div>
-             <a href="../pages/Error404.html" class="btn btn-primary btn-lg fw-light">Comprar</a>
+             <a href="../pages/Error404.html" class=" mt-5 btn btn-primary btn-lg fw-light">Comprar</a>
            </div>
          </div>
         </div>`;
